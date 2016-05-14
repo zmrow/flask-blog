b90889f928cc7b2111b7cc667d49115bd81c8cf8
zmrowicki@hotmail.com <zmrowicki@hotmail.com>
2016-05-14 15:32:53 -0700

    Add views to controller, create 3 html templates

--- blog.py
+++ blog.py
@@ -10,9 +10,18 @@ DATABASE = 'blog.db'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-# connect to db
+# Connect to db
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+# Views
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
