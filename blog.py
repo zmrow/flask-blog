1051c7d358d3ae8b64a11839d95a1d67e00e8982
zmrowicki@hotmail.com <zmrowicki@hotmail.com>
2016-05-16 20:07:03 -0700

    Add login decorator.  Add form to add new posts.  Add simple styles

--- blog.py
+++ blog.py
@@ -48,10 +48,31 @@ def logout():
     flash('You were logged out')
     return redirect(url_for('login'))
 
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash("All fields are required. Please try again.")
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute('insert into posts (title, post) values (?,?)', 
+                [request.form['title'], request.form['post']])
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully posted!')
+        return redirect(url_for('main'))
+
 @app.route('/main')
 @login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    curr = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], post=row[1]) for row in curr.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
